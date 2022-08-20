1b7a082534113bbf082c0e74565b07d3c9fda1d0
Iqbal <iqbal@Iqbals-MacBook-Pro.local>
2022-08-20 19:35:05 +0800

    update search website

--- consumers/search/src/Pages/Search.tsx
+++ consumers/search/src/Pages/Search.tsx
@@ -3,18 +3,39 @@ import {useState} from 'react'
 import { Container, Row, Col } from 'react-grid-system';
 import { InputGroup, Icon, Button } from '@blueprintjs/core'
 import { get_products } from '../_aqua/node';
+import ListedProductBox from '../Components/ListedProductBox';
+import { ethers } from 'ethers'
+import AssetWrapperAbi from "../artifacts/AssetWrapper.json"
+export  interface AssetModel {
+  address: string
+  id: string
+  title: string
+  price: number
+}
 
 const Search = () => {
 
   const [search, setSearchText] = useState('')
-  const [products, setProducts] = useState<[]>([])
+  const [products, setProducts] = useState<AssetModel[]>([])
 
   const onSearchChanged =(event: any) => setSearchText(event.target.value)
 
   const onHandleSearch = async () => {
-    console.log('hello')
-    let res = await get_products("12D3KooWLAH8YNvtwxgfETy65Xm2RHZ9rPBGUfstBXHkfgk8dwvA")
-    console.log(res)
+    let res: AssetModel[] = await get_products("12D3KooWL1sFtSPFxpQefntrmxQqkFc2NPBjeWZwR1PTX5xbkotF")
+    setProducts(res)
+  }
+
+  const onHandlePurchase = async (asset: AssetModel) => {
+    if(!(window as any).ethereum) { return }
+
+    const provider = new ethers.providers.Web3Provider((window as any).ethereum, "any");
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(process.env.REACT_APP_CONTRACT_ASSET_WRAPPER as string, AssetWrapperAbi.abi, signer)
+      await contract.buy(
+        parseInt(asset.id)
+      )
   }
 
   return (
@@ -35,6 +56,18 @@ const Search = () => {
         </Col>
       </Row>
       <Row style={{ height: '40px' }} align="center">
+        <Col sm={1}>&nbsp;</Col>
+        {products.map((asset) => {
+          return (
+            <Col sm={2} key={asset.id} className="mt-xl">
+              <ListedProductBox
+                title={asset.title as string}
+                description={`Bundle #${asset.id}`}
+                onClick={() => onHandlePurchase(asset)} />
+            </Col>
+          )
+        })}
+        <Col sm={1}>&nbsp;</Col>
       </Row>
 
     </Container>
