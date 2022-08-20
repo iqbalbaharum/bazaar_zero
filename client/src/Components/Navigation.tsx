492064a9e13c32c5baa51b802265c4f5693b602b
JF <malaysiaklcc@gmail.com>
2022-08-20 20:57:56 +0800

    navigation & myshop UI glow up

--- client/src/Components/Navigation.tsx
+++ client/src/Components/Navigation.tsx
@@ -3,11 +3,15 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Alignment, Classes, Navbar, NavbarGroup, NavbarDivider, Button, NavbarHeading, Icon } from '@blueprintjs/core'
 import useSequence from '../Hook/useSequence';
+import { ETHAuth } from '@0xsequence/ethauth'
+import { sequence } from '0xsequence'
 import { useEthers } from '@usedapp/core';
 
 import { Fluence } from '@fluencelabs/fluence';
 import { krasnodar } from '@fluencelabs/fluence-network-environment';
 import FluenceService from './FluenceService';
+
+import logo from '../assets/bazaar-zer0-logo.png';
 
 const Navigation = () => {
 
@@ -58,15 +62,45 @@ const Navigation = () => {
     )
   }
 
+  const onHandleSequenceWallet = async () => {
+    const wallet = sequence.getWallet()
+    const connectDetails = await wallet.connect({
+      app: 'Bazaar Zero',
+      authorize: true,
+      settings: {
+        theme: 'indigoDark',
+        bannerUrl: ``,
+        includedPaymentProviders: ['moonpay'],
+        defaultFundingCurrency: 'matic',
+        defaultPurchaseAmount: 111
+      }
+    })
+
+    const ethAuth = new ETHAuth()
+
+    if (connectDetails.proof) {
+      const decodedProof = await ethAuth.decodeProof(connectDetails.proof.proofString, true)
+
+      const isValid = await wallet.utils.isValidTypedDataSignature(
+        await wallet.getAddress(),
+        connectDetails.proof.typedData,
+        decodedProof.signature,
+        await wallet.getAuthChainId()
+      )
+
+      if (!isValid) throw new Error('sig invalid')
+    }
+  }
+
   return (
     <Navbar>
       <NavbarGroup align={Alignment.LEFT}>
-        <Button className={Classes.MINIMAL} text="My Shop" onClick={() => navigate('/my')} />
-        <Button className={Classes.MINIMAL} text="Wrapper" onClick={() => navigate('/wrapping')}/>
+        <img height={40} src={logo} alt="Bazaar Zero" onClick={() => navigate('/my')}/>
+        {/* <Button className={Classes.MINIMAL} text="Wrapper" onClick={() => navigate('/wrapping')}/> */}
         <NavbarDivider />
-        Sequence: <br />{account ? account : <Button intent="success" text="Login" onClick={() => navigate('/login')} /> }
+        Sequence <span style={{marginLeft: 4, marginRight: 4}}>:</span> {account ? account : <Button intent="primary" text="Login" onClick={onHandleSequenceWallet} /> }
         <NavbarDivider />
-        Polygon: <br />{ethers.account ? ethers.account : <Button large={true} intent="success" text="Connect Metamask" onClick={() => ethers.activateBrowserWallet()} /> }
+        Polygon <span style={{marginLeft: 4, marginRight: 4}}>:</span> {ethers.account ? ethers.account : <Button intent="primary" text="Connect Metamask" onClick={() => ethers.activateBrowserWallet()} /> }
         <NavbarDivider />
         <NavbarHeading>{!isFluenceConnected ? <ConnectingText /> : <ConnectedText />}</NavbarHeading>
       </NavbarGroup>
